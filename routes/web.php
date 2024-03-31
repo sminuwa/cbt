5ccca28aecdb39de90f812117cad607cfe3c0ba7
A. L. Zailani <devpsys@gmail.com>
2024-03-31 15:12:24 +0100

    qwerty

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/form', [QuestionController::class, 'author'])->name('author');
+Route::post('/form', [QuestionController::class, 'authorPost'])->name('author.post');
