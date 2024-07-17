3a3778cb219b14c80a87c025a159fc57543ce9ef
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-07-17 22:42:17 +0100

    bug

--- app/Helper.php
+++ app/Helper.php
@@ -26,30 +26,20 @@ class Helper
             // Remove the difficulty indicator from the question text
             $questionText = preg_replace('/\{[SMD]\}/', '', $questionText);
             $questionText = str_replace('<br>', '', $questionText);
-<<<<<<< HEAD
             $questionText = strip_tags(str_replace('&nbsp;', '', $questionText),'<img>');
-=======
-            $questionText = str_replace('&nbsp;', '', $questionText);
             $questionText = str_replace('</span></p> <p class="MsoNormal"><span lang="EN">', '', $questionText);
->>>>>>> 2d58a3fd0fee69c6ba113a7738ad03da7034503d
+
             // Remove the last element (empty string) from the array
 //            array_pop($parts);
 
             $options = [];
             foreach ($parts as $option) {
                 $isCorrect = strpos($option, $delimeters['answer']) !== false;
-<<<<<<< HEAD
                 $optionText = strip_tags(trim(str_replace($delimeters['answer'], '', $option)),'<img>');
                 $optionText = strip_tags(trim(str_replace('<br>', '', $optionText)),'<img>');
                 $optionText = strip_tags(trim(str_replace('&nbsp;', '', $optionText)),'<img>');
                 $optionText = strip_tags(trim(str_replace('<br style="mso-special-character: line-break;"><!--[endif]--></span></p>', '', $optionText)),'<img>');
-=======
-                $optionText = trim(str_replace($delimeters['answer'], '', $option));
-                $optionText = trim(str_replace('<br>', '', $optionText));
-                $optionText = trim(str_replace('&nbsp;', '', $optionText));
-                $optionText = trim(str_replace('<br style="mso-special-character: line-break;"><!--[endif]--></span></p>', '', $optionText));
-                $optionText = trim(str_replace('</span></p> <p class="MsoNormal"><span lang="EN">', '', $optionText));
->>>>>>> 2d58a3fd0fee69c6ba113a7738ad03da7034503d
+
                 $options[] = (object)[
                     'text' => $optionText,
                     'isCorrect' => $isCorrect
