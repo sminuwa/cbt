32b7c535bd60fa2e5561440fe5b841f76d55059e
A. L. Zailani <devpsys@gmail.com>
2024-08-04 18:29:37 +0100

    report summary: revisited

--- resources/views/pages/admin/reports/ajax/report-summary.blade.php
+++ resources/views/pages/admin/reports/ajax/report-summary.blade.php
@@ -1,35 +1,50 @@
 <div class="table-responsive">
-    <table id="report" class="table table-bordered table-stripped table-condensed">
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Indexing</th>
-            <th>Surname</th>
-            <th>First Name</th>
-            <th>Other Name(s)</th>
-            {{-- <th>Paper Code</th> --}}
-            @foreach ($subjects as $subject)
-                <th>{{ $subject }}</th>
-            @endforeach
-            {{-- <th>Action</th> --}}
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($reports as $report)
+    @if(count($reports)>0)
+        <table id="report" class="table table-bordered table-stripped table-condensed">
+            <thead>
             <tr>
-                <td>{{$loop->iteration}}</td>
-                <td>{{$report['indexing']}}</td>
-                <td>{{$report['surname']}}</td>
-                <td>{{$report['firstname']}}</td>
-                <td>{{$report['other_names']}}</td>
-                {{-- <td>{{$report->subject_code}}</td> --}}
+                <th>#</th>
+                <th>Indexing</th>
+                <th>Surname</th>
+                <th>First Name</th>
+                <th>Other Name(s)</th>
+                {{-- <th>Paper Code</th> --}}
                 @foreach ($subjects as $subject)
-                  <td>{{ $report[$subject] }}</td>
+                    <th>{{ $subject }}</th>
                 @endforeach
-                {{-- <td>{{number_format($report->score * $report->aggregate, 2)}}</td> --}}
-                {{-- <td></td> --}}
+                {{-- <th>Action</th> --}}
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($reports as $report)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$report['indexing']}}</td>
+                    <td>{{$report['surname']}}</td>
+                    <td>{{$report['firstname']}}</td>
+                    <td>{{$report['other_names']}}</td>
+                    {{-- <td>{{$report->subject_code}}</td> --}}
+                    @foreach ($subjects as $subject)
+                    <td>{{ $report[$subject] }}</td>
+                    @endforeach
+                    {{-- <td>{{number_format($report->score * $report->aggregate, 2)}}</td> --}}
+                    {{-- <td></td> --}}
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="card border-info">
+            <div class="card-header">
+                <h4 class="card-title">&nbsp;</h4>
+            </div>
+            <div class="card-body pt-2 pb-2  mt-1 mb-1">
+                <div class="row">
+                    <div class="row pb-5 pt-5">
+                        <p class="text-center"> No record found</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
