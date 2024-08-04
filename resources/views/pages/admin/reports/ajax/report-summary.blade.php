3b2e9ac1386316e862e01bff11a4e29258c13b8b
A. L. Zailani <devpsys@gmail.com>
2024-08-04 18:17:57 +0100

    report summary revisited

--- resources/views/pages/admin/reports/ajax/report-summary.blade.php
+++ resources/views/pages/admin/reports/ajax/report-summary.blade.php
@@ -1,24 +1,33 @@
 <div class="table-responsive">
-    <table id="report" class="table table-bordered">
+    <table id="report" class="table table-bordered table-stripped table-condensed">
         <thead>
         <tr>
             <th>#</th>
             <th>Indexing</th>
-            <th>Candidate Name</th>
-            <th>Paper Code</th>
-            <th>Score</th>
-            <th>Action</th>
+            <th>Surname</th>
+            <th>First Name</th>
+            <th>Other Name(s)</th>
+            {{-- <th>Paper Code</th> --}}
+            @foreach ($subjects as $subject)
+                <th>{{ $subject }}</th>
+            @endforeach
+            {{-- <th>Action</th> --}}
         </tr>
         </thead>
         <tbody>
         @foreach($reports as $report)
             <tr>
-                <th>{{$loop->iteration}}</th>
-                <td>{{$report->indexing}}</td>
-                <td>{{$report->name}}</td>
-                <td>{{$report->subject_code}}</td>
-                <td>{{number_format($report->score * $report->aggregate, 2)}}</td>
-                <td></td>
+                <td>{{$loop->iteration}}</td>
+                <td>{{$report['indexing']}}</td>
+                <td>{{$report['surname']}}</td>
+                <td>{{$report['firstname']}}</td>
+                <td>{{$report['other_names']}}</td>
+                {{-- <td>{{$report->subject_code}}</td> --}}
+                @foreach ($subjects as $subject)
+                  <td>{{ $report[$subject] }}</td>
+                @endforeach
+                {{-- <td>{{number_format($report->score * $report->aggregate, 2)}}</td> --}}
+                {{-- <td></td> --}}
             </tr>
         @endforeach
         </tbody>
