fc95e927821e8948986d36040bcad7106e116f0a
Sunusi M. Inuwa <sminuwa@yahoo.com>
2024-09-20 19:59:44 +0100

    afad

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -128,6 +128,8 @@ class MiscController extends Controller
                     ]
                 ]
             ];
+
+            return $netplan_config;
             
             yaml_emit_file('/etc/netplan/'.$file_name, $netplan_config, YAML_ANY_ENCODING, 4);
             return back()->with('success', 'Server configured successfully');
